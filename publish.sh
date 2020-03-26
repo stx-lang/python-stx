551c9a3427f978986ade84ad9d8280ec9fd7ad6a
sergiouph <sergio.pedraza@wizeline.com>
2020-03-25 20:28:08 -0600

    Update publish script

---
+++ publish.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+VERSION=$(cat version.txt)
+
+echo "Clear previous builds..." && \
+./clear.sh && \
+
+echo "Create a new brand virtual env..." && \
+python -m venv env && \
+
+echo "Activating virtual env..." && \
+source env/bin/activate && \
+
+echo "Installing requirements..." && \
+pip install -r requirements-dev.txt && \
+
+echo "Auditing code..." && \
+flake8 && \
+
+echo "Generating dist package..." && \
+python setup.py sdist bdist_wheel && \
+
+echo "Uploading version $VERSION..." && \
+twine upload dist/* && \
+
+echo "Creating tag..." && \
+git tag $VERSION && \
+git push origin $VERSION && \
+
+echo "Done!"
